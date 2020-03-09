6d18b5cc69f640767cdd6ab9920ced82865659d2
Your Name <you@example.com>
2020-03-09 04:20:54 -0400

    complete

--- main.js
+++ main.js
@@ -15,14 +15,41 @@ return arr
 }
 
 const reversify = function(str){
-  let new = '';
+  let newS= '';
   for (let i = str.length - 1; i >= 0; i-- ){
-    new = new + str[i];
+    newS = newS + str[i];
   }
-  return new ;
+  return newS ;
+}
+
+const titleify = function(str){
+  let newStr = '';
+  for (let i = 0; i < str.length; i++ ){
+    if(str[i-1] === ' ' || i === 0){
+      newStr = newStr + str[i].toUpperCase();
+    } else {
+    newStr = newStr + str[i].toLowerCase();
+    }
+  }
+return newStr;
 }
 
 
+const crazify = function(str){
+  let result = '';
+  let count = 1;
+    for (let i = 0; i < str.length; i++){
+    if (str[i] !== ' '){
+        count++;
+      }
+    if(count % 2 === 1){
+        result = result + str[i].toUpperCase();
+  } else {
+        result = result + str[i].toLowerCase();
+      }
+    }
+    return result; 
+  }
 
 
 if (typeof biggify === 'undefined') {
